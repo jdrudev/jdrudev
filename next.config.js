e07f7d2a8d3c28bc5d1c72fe5024dc304db387cc
jdru <34028017+jdrudev@users.noreply.github.com>
2026-07-28 20:17:30 +0100

    Delete next.config.js
    no longer needed

--- next.config.js
+++
@@ -1,9 +0,0 @@
-const nextConfig = {
-  output: 'export',
-  // If you use next/image, you may need to disable image optimization:
-  // images: { unoptimized: true },
-  // Set basePath to your repository name if hosting under a subdirectory:
-  // basePath: '/your-repo-name',
-};
-
-module.exports = nextConfig;   
