2e45b99116568b88f07ae2d676713f52add75198
jdru <34028017+jdrudev@users.noreply.github.com>
2026-07-28 17:57:15 +0100

    Create next.config.js

---
+++ next.config.js
@@ -0,0 +1,9 @@
+const nextConfig = {
+  output: 'export',
+  // If you use next/image, you may need to disable image optimization:
+  // images: { unoptimized: true },
+  // Set basePath to your repository name if hosting under a subdirectory:
+  // basePath: '/your-repo-name',
+};
+
+module.exports = nextConfig;   
